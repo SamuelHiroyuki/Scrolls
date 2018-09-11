6bdd2255cd31f89d47150a45e335a0614d5cec80
PuzzleSolutions <42379593+PuzzleSolutions@users.noreply.github.com>
2018-09-11 11:58:18 -0300

    11/09

---
+++ Aplicação/Scrolls E-commerce/Scrolls/Scrolls/Scripts/script.js
@@ -0,0 +1,22 @@
+function trocaCor(){
+	document.getElementById("lupa").style.color = "#F8C141";
+}
+
+function retrocaCor(){
+	document.getElementById("lupa").style.color = "#F8C141";
+}
+
+    window.onload=function(){
+      
+        $(document).ready(function() {
+          $(window).scroll(function() {
+            if ($(document).scrollTop() > 200) {
+              $('#nav').addClass('shrink');
+            } else {
+              $('#nav').removeClass('shrink');
+            }
+          });
+        });
+        
+        
+            }
