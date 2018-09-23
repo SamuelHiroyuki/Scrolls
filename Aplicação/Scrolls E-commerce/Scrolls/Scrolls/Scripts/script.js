b8934abcfc81ef63e2c87b631cb9f589bca88cde
OliveiraMateus <marcianooliveirasilva@gmail.com>
2018-09-23 13:01:37 -0300

    23/09
    Layout completamente ajustado.
    Obs: ainda sujeito a alterações

--- Aplicação/Scrolls E-commerce/Scrolls/Scrolls/Scripts/script.js
+++ Aplicação/Scrolls E-commerce/Scrolls/Scrolls/Scripts/script.js
@@ -1,22 +1,17 @@
-function trocaCor(){
-	document.getElementById("lupa").style.color = "#F8C141";
-}
-
-function retrocaCor(){
-	document.getElementById("lupa").style.color = "#0D7694";
-}
-
-    window.onload=function(){
-      
-        $(document).ready(function() {
-          $(window).scroll(function() {
-            if ($(document).scrollTop() > 200) {
-              $('#nav').addClass('shrink');
-            } else {
+window.onload=function(){
+  $(document).ready(function() {
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 200) {
+        $('#nav').addClass('shrink');
+      } else {
               $('#nav').removeClass('shrink');
-            }
-          });
-        });
-        
-        
-            }
+        }
+    });
+  });
+}
+
+if (window.parent && window.parent.parent) {
+  window.parent.parent.postMessage(["resultsFrame", {
+    height: document.body.getBoundingClientRect().height,
+    slug: ""}], "*")
+}
