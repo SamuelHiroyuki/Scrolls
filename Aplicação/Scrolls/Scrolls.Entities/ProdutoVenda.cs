588732122e9cfa0a65cd76087d57d6867045d354
Samuel Hiroyuki <samduasvezesmortal@gmail.com>
2018-09-26 17:39:21 -0300

    26/09
    Realizada a remoção de alguns commits errados e breve organização das pastas

---
+++ Aplicação/Scrolls/Scrolls.Entities/ProdutoVenda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrolls.Entities
+{
+    public class ProdutoVenda
+    {
+        public virtual Venda Venda { get; set; }
+        public int VendaId { get; set; }
+        public virtual Produto Produto { get; set; }
+        public int ProdutoId { get; set; }
+
+        [Required, RegularExpression("^[^ -][0 - 9]$", ErrorMessage = "Somente números inteiros são aceitos."), Range(1, 50, ErrorMessage = "A quantidade deve ser de 1-50 unidades.")]
+        public int Quantidade { get; set; }
+
+        [Required]
+        public decimal PrecoVenda { get; set; }
+    }
+}
---
+++ Mobile/Scrolls2/app/src/main/java/com/example/lucasclaro/scrolls2/Cadastro.java
@@ -0,0 +1,50 @@
+package com.example.lucasclaro.scrolls2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class Cadastro extends AppCompatActivity {
+
+    Button btnCriarConta;
+    TextView txtNome;
+    TextView txtSobrenome;
+    TextView txtEmail;
+    TextView txtSenha;
+    public BancoControler dao = new BancoControler(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        btnCriarConta = (Button)findViewById(R.id.btnCriarConta);
+        txtNome = (TextView)findViewById(R.id.txtNome);
+        txtSobrenome = (EditText) findViewById(R.id.txtSobrenome);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        txtSenha = (EditText) findViewById(R.id.txtSenha);
+    }
+
+    public void CadastroContinuar(View view){
+        String Email =txtEmail.getText().toString();
+        String Senha =txtSenha.getText().toString();
+        String Nome =txtNome.getText().toString();
+        String Sobrenome =txtSobrenome.getText().toString();
+        String resp = dao.CadastraUsuario(Nome, Sobrenome, Email, Senha);
+        Toast.makeText(this,resp, Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, Principal.class);
+        startActivity(intent);
+    }
+
+    public void CadastroLogin(View view){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+}
