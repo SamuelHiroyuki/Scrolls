2468009b19638de12f1b875f1c370018d0397545
Samuel Hiroyuki <samduasvezesmortal@gmail.com>
2018-10-13 17:49:33 -0300

    13/10

--- Aplicação/Scrolls/Scrolls.Manager/Controllers/HomeController.cs
+++ Aplicação/Scrolls/Scrolls.Manager/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Scrolls.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,11 @@ namespace Scrolls.Manager.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult Login(Funcionario f) {
+            
             return View();
         }
     }
