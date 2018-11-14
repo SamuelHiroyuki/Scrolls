22a4e5450addd4a2350301eabf1c54b6d3eab7c1
PuzzleSolutions <42379593+PuzzleSolutions@users.noreply.github.com>
2018-11-14 11:46:33 -0200

    14/11
    quase pronto

--- Prototipo Scrolls/Prototipo/Script.js
+++ Prototipo Scrolls/Prototipo/Script.js
@@ -1,10 +1,4 @@
 (function($) {
-
-  // :: Nicescroll Active Code
-    if ($.fn.niceScroll) {
-        $(".cart-list, .cart-content").niceScroll();
-    }
-
   // NAVIGATION
   var responsiveNav = $('#responsive-nav'),
     catToggle = $('#responsive-nav .category-nav .category-header'),
@@ -38,7 +32,113 @@
       }
     }
   });
+        
+//HOME SLICK
+$('#home-slick').slick({
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    arrows: true,
+});
+
+// PRODUCTS SLICK
+$('#product-slick-1').slick({
+    slidesToShow: 4,
+    slidesToScroll:  4,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    dots: true,
+    arrows: false,
+    appendDots: '.product-slick-dots-1',
+    responsive: [{
+        breakpoint: 991,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            dots: false,
+            arrows: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        }
+        },
+    ]
+});
+
+$('#product-slick-2').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    dots: true,
+    arrows: false,
+    appendDots: '.product-slick-dots-2',
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            dots: false,
+            arrows: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        }
+        },
+    ]
+});
+     
+$('#product-slick-3').slick({
+    slidesToShow: 4,
+    slidesToScroll:  4,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    dots: true,
+    arrows: false,
+    appendDots: '.product-slick-dots-3',
+    responsive: [{
+        breakpoint: 991,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+        breakpoint: 480,
+            settings: {
+                dots: false,
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+    ]
+});
 })(jQuery);
+
+$(document).ready(function () {
+    $("#product-main-view").owlCarousel({
+        navigation: true,
+        slideSpeed: 300,
+        loop: true,
+        nav: true,
+        navText: ["<i class='icon ion-md-arrow-round-back'><i>", "<i class='icon ion-md-arrow-round-forward'><i>"],
+        paginationSpeed: 400,
+        singleItem: true,
+        items: 1,
+    });
+});
 
 var cartbtn1 = $('#essenceCartBtn');
 var cartOverlay = $(".cart-bg-overlay");
