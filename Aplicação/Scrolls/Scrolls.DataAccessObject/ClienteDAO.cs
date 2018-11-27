b5dfebfc02a1a6440b28c66bb38ccc17f553b445
OliveiraMateus <marcianooliveirasilva@gmail.com>
2018-11-26 23:16:48 -0200

    26/11
    cadastro e login cliente pronto.
    Obs: falta testar se ta tudo certo e colocar as validações

---
+++ Aplicação/Scrolls/Scrolls.DataAccessObject/ClienteDAO.cs
@@ -0,0 +1,49 @@
+﻿using Scrolls.Database;
+using Scrolls.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrolls.DataAccessObject
+{
+    public class ClienteDAO
+    {
+        private EntitiesContext context;
+         public ClienteDAO()
+        {
+            context = new EntitiesContext();
+        }
+
+        public void Cadastrar(Cliente c)
+        {
+            context.Clientes.Add(c);
+            context.SaveChanges();
+        }
+
+        public Cliente BuscarId(int id)
+        {
+            return context.Clientes.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Atualizar()
+        {
+            context.SaveChanges();
+        }
+
+        public Cliente Login(string pass, string ema)
+        {
+
+            Cliente cliente = context.Clientes.FirstOrDefault(c => c.Email.Equals(ema) && c.Senha.Equals(pass));
+            if (cliente != null)
+            {
+                return cliente;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Aplicação/Scrolls/Scrolls.Web/Controllers/ClienteController.cs
@@ -0,0 +1,26 @@
+﻿using Scrolls.DataAccessObject;
+using Scrolls.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scrolls.Web.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult JoinPage(Cliente cliente)
+        {
+            ClienteDAO cdao = new ClienteDAO();
+            cdao.Cadastrar(cliente);
+            return View();
+        }
+    }
+}
